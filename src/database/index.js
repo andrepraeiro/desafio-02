00ba49b1d94f3be3759a9853757f1e10386431d0
Andre Praeiro <andrepraeiro@gmail.com>
2019-08-23 23:16:24 -0400

    initial commit

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import Sequelize from 'sequelize';
+
+import DatabaseConfig from '../config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(DatabaseConfig);
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
